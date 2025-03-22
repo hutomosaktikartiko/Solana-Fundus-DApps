e26c19b158c322a06f33333f485f58fb74c0667f
Hutomo Sakti Kartiko <hutomosaktikartiko.dev@gmail.com>
2025-03-22 13:45:48 +0700

    feat: anchor - initalize states

--- anchor/programs/fundus/src/lib.rs
+++ anchor/programs/fundus/src/lib.rs
@@ -6,6 +6,7 @@ pub mod instructions;
 pub mod states;
 
 use instructions::*;
+#[allow(unused_imports)]
 use states::*;
 
 // Program ID declaration (replace with your own ID when deploying)
@@ -14,4 +15,8 @@ declare_id!("5Z1U7HwvHvLyaSqcKAa9VrydcVoDGhT6NC8SWMqERB6A");
 #[program]
 pub mod fundus {
     use super::*;
+
+    pub fn initialize(ctx: Context<InitalizeCtx>) -> Result<()> {
+        instructions::initialize(ctx)
+    }
 }
